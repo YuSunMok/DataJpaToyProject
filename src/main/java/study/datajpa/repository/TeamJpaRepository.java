1abd1156c89342173518bf1a8e46962a7158ee21
sunmok <tjsahr9191@naver.com>
2023-12-08 11:27:41 +0900

    feat : TeamRepository by Spring Data JPA

--- src/main/java/study/datajpa/repository/TeamJpaRepository.java
+++ src/main/java/study/datajpa/repository/TeamJpaRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class TeamJPARepository {
+public class TeamJpaRepository {
 
     @PersistenceContext
     private EntityManager em;
